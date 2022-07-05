9a9f712522d114c4bd5f260308459952453ef128
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2022-07-05 20:07:05 +0900

    프롭 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-function Header() {
+function Header(props) {
+  console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
 function Nav() {
@@ -14,34 +15,19 @@ function Nav() {
     </ol>
   </nav>
 }
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
-      <Header></Header>
+      <Header title="REACT"></Header>
       <Nav></Nav>
-      <Article></Article>
+      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Article title="Hi" body="Hello, React"></Article>
     </div>
   );
 }
